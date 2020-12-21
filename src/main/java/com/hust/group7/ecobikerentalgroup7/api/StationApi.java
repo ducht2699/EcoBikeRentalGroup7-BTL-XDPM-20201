e6e6e7f2911b26d9f77ffa93a214a94fd7e1a544
anonymousid24111 <anonymousid2411@gmail.com>
2020-12-21 18:06:01 +0700

    giang update station api

--- src/main/java/com/hust/group7/ecobikerentalgroup7/api/StationApi.java
+++ src/main/java/com/hust/group7/ecobikerentalgroup7/api/StationApi.java
@@ -61,4 +61,40 @@ public class StationApi {
 		 statement.executeUpdate(sqlString, Statement.RETURN_GENERATED_KEYS);
 	     ResultSet rs = statement.getGeneratedKeys();
 	}
+	public static Station getStationById(Integer stationId) throws SQLException {
+		Statement statement = DataBase.connectionStatic.createStatement();
+		Station stations = null;// = new Station();
+		String sqlString = "select * from stations where id='"+stationId+"'";
+        ResultSet rs = statement.executeQuery(sqlString);
+        System.out.println(rs);
+        while (rs.next()) {
+        	Statement statement1 = DataBase.connectionStatic.createStatement();
+        	ResultSet rsNumberOfBikes = statement1.executeQuery("select count(*) as numberOfBikes from bikes where type='Bike' and station_id="+ rs.getInt("id"));
+        	int numberOfBikes = 0,numberOfEBikes = 0,numberOfTwinBikes = 0,numberOfEmptyDocks;
+        	if(rsNumberOfBikes.next()) {
+        		
+        		numberOfBikes = rsNumberOfBikes.getInt("numberOfBikes");
+        	}
+        	Statement statement2 =DataBase.connectionStatic.createStatement();
+        	ResultSet rsNumberOfEBikes = statement2.executeQuery("select count(*) as numberOfEBikes from bikes where type='EBike' and station_id="+ rs.getInt("id"));
+        	if(rsNumberOfEBikes.next()) {
+        		
+        		numberOfEBikes = rsNumberOfEBikes.getInt("numberOfEBikes");
+        	}
+        	Statement statement3 =DataBase.connectionStatic.createStatement();
+        	ResultSet rsNumberOfTwinBikes = statement3.executeQuery("select count(*) as numberOfTwinBikes from bikes where type='Bike' and station_id="+ rs.getInt("id"));
+        	if(rsNumberOfTwinBikes.next()) {
+        		
+        		numberOfTwinBikes = rsNumberOfTwinBikes.getInt("numberOfTwinBikes");
+        	}
+        	int numberOfDocks = rs.getInt("number_of_docks");
+        	numberOfEmptyDocks = numberOfDocks - (numberOfBikes+numberOfEBikes+numberOfTwinBikes); 
+        	System.out.println(numberOfBikes);
+        	System.out.println(numberOfEBikes);
+        	System.out.println(numberOfTwinBikes);
+        	System.out.println(numberOfEmptyDocks);
+            stations = new Station(rs.getInt("id"), rs.getString("name"), rs.getString("address"), numberOfDocks, rs.getFloat("distance_to_walk"), rs.getFloat("time_to_walk"), numberOfBikes, numberOfEBikes, numberOfTwinBikes, numberOfEmptyDocks);
+        }
+        return stations;
+	}
 }
