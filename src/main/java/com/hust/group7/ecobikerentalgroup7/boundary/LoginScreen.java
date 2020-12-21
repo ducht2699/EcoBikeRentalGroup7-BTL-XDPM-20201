5227730cee1eccf2f40f11bfb7f851e022fb66f8
Duc Hoang <61897723+ducht2699@users.noreply.github.com>
2020-12-21 12:26:56 +0700

    - Add Admin edit bikes

--- src/main/java/com/hust/group7/ecobikerentalgroup7/boundary/LoginScreen.java
+++ src/main/java/com/hust/group7/ecobikerentalgroup7/boundary/LoginScreen.java
@@ -46,7 +46,7 @@ public class LoginScreen extends javax.swing.JFrame {
 
         passwordLabel.setText("Password");
 
-        usernameField.setText("luatnc");
+        usernameField.setText("ducht");
 
         loginButton.setText("Login");
         loginButton.addActionListener(new java.awt.event.ActionListener() {
@@ -65,7 +65,7 @@ public class LoginScreen extends javax.swing.JFrame {
             }
         });
 
-        passwordField.setText("nchust");
+        passwordField.setText("hthust");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
